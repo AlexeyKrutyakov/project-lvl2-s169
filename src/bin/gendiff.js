6da2a0ea9f81ea7ed7362d8d56fac3ddf16a4e55
arctikbear <coder.arctik@gmail.com>
2017-12-18 12:46:43 +0300

    add autohelp in bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-import gendiff from '..';
+const program = require('commander');
+
+program
+  .version('0.0.1')
+  .usage('Usage: gendiff [options] <firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
