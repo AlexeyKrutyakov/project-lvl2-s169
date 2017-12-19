ded55f31a958db5baf1b23aaf4211232010e8009
arctikbear <coder.arctik@gmail.com>
2017-12-19 17:04:52 +0300

    fix bin file
    replace .parse(process.argv) to end of program

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import program from 'commander';
-import { gendiff } from '..';
+import gendiff from '..';
 
 program
-  .version('0.1.0')
+  .version('0.1.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
-  .parse(process.argv)
   .arguments('<file1>, <file2>')
   .action((file1, file2) => {
     const result = gendiff(file1, file2);
     console.log(result);
-  });
+  })
+  .parse(process.argv);
